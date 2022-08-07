fd63f67af4739b2bc8b9bd64ac421b38d38638cb
viettrung2103 <viettrung21@gmail.com>
2022-08-07 09:06:13 +0300

    nav bar and routing complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FrontpageComponent } from './components/frontpage/frontpage.component';
+import { LivestreamsComponent } from './components/livestreams/livestreams.component';
+import { MatchesComponent } from './components/matches/matches.component';
+import { StandingsComponent } from './components/standings/standings.component';
+import { TeamsComponent } from './components/teams/teams.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'frontpage', component: FrontpageComponent },
+  { path: 'livestreams', component: LivestreamsComponent },
+  { path: 'matches', component: MatchesComponent },
+  { path: 'teams', component: TeamsComponent },
+  { path: 'standings', component: StandingsComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, //if there is no address, redirect to frontpage
+  { path: '**', component: FrontpageComponent, pathMatch: 'full' }, // default page if visit wrong address
+  { path: '**', component: FrontpageComponent, pathMatch: 'full' }, // default page if visit wrong address
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FrontpageComponent } from './components/frontpage/frontpage.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { PlayersComponent } from './components/players/players.component';
+import { UsersComponent } from './components/users/users.component';
+import { StandingsComponent } from './components/standings/standings.component';
+import { LivestreamsComponent } from './components/livestreams/livestreams.component';
+import { MatchesComponent } from './components/matches/matches.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    FrontpageComponent,
+    TeamsComponent,
+    PlayersComponent,
+    UsersComponent,
+    StandingsComponent,
+    LivestreamsComponent,
+    MatchesComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
