47e7fd2c3b3559907049db3b9803dd89a7bcb384
viettrung2103 <viettrung21@gmail.com>
2022-08-21 03:32:24 +0300

    add disable to team Detail UI, will continue with create and edit tomorrow

--- src/app/components/teams/components/team-detail/team-detail.component.ts
+++ src/app/components/teams/components/team-detail/team-detail.component.ts
@@ -11,6 +11,9 @@ import { TeamApiService } from 'src/app/services/team-api.service';
 export class TeamDetailComponent implements OnInit {
   public team: Team | undefined;
   public regions: string[] = Object.keys(RegionType);
+  mode: 'EDIT' | 'LOCKED' = 'LOCKED'; //DEFAULT IS LOCKED
+  buttonText: 'SAVE CHANGES' | 'EDIT' = 'EDIT'; //DEFAULT IS EDIT
+  // public mode: editMode;
 
   constructor(
     private activateRoute: ActivatedRoute,
@@ -31,5 +34,24 @@ export class TeamDetailComponent implements OnInit {
           this.team = teamDetailData; // by assign our team to the result API, we can then interpolate the data to our view or HTML
         });
     });
+  }
+
+  changeMode(mode?: 'EDIT' | 'LOCKED'): void {
+    // if the mode is locked, click the     button to change the mode to edit
+    if (this.mode === 'LOCKED') {
+      console.log('Pressing button');
+      this.mode = 'EDIT';
+      this.changeButtonText(this.buttonText);
+      console.log(`this is mode: ${this.mode}`);
+    } else {
+      console.log('Pressing button');
+      this.mode = 'LOCKED';
+      this.changeButtonText(this.buttonText);
+      console.log(`This is mode: ${this.mode}`);
+    }
+  }
+  changeButtonText(buttonText?: 'EDIT' | 'SAVE CHANGES'): void {
+    this.buttonText = this.buttonText === 'EDIT' ? 'SAVE CHANGES' : 'EDIT';
+    //Logic to update the user on the back end
   }
 }
---
+++ src/app/models/interfaces/edit.interface.ts
@@ -0,0 +1,8 @@
+// export enum EditType {
+//   EDIT = 'EDIT', //can edit
+//   LOCKED = 'LOCKED', // cannot edit in this mode
+// }
+
+// export interface editMode {
+//   editMode: EditType;
+// }
